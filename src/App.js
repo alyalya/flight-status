4d67b1a0b7959bb5d37d9c79488134fd197a07d6
Aliya <doryfshr@icloud.com>
2018-11-11 23:35:30 +0300

    Added API fetching

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
 
     this.filterType = this.filterType.bind(this);
     this.filterFlights = this.filterFlights.bind(this);
+    this.filterStatus = this.filterStatus.bind(this);
 
+    // Test data
     this.initial = {
       data: [{
         "type": "arrival",
@@ -364,7 +366,7 @@ class App extends Component {
       },
       {
         "type": "arrival",
-        "status": "cancelled",
+        "status": "scheduled",
         "departure": {
           "iataCode": "ULY",
           "icaoCode": "KJFK",
@@ -447,440 +449,24 @@ class App extends Component {
     }
 
     this.state = {
-      data: [{
-        "type": "arrival",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "JFK",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "landed",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "CVG",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "432302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "arrival",
-        "status": "landed",
-        "departure": {
-          "iataCode": "ULY",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-13-11T01:08:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-03-11T06:30:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Gatlas Gir",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "354602",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "CVG",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "432302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "arrival",
-        "status": "landed",
-        "departure": {
-          "iataCode": "ULY",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-13-11T01:08:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-03-11T06:30:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Gatlas Gir",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "354602",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "arrival",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "ULY",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-13-11T01:08:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-03-11T06:30:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Gatlas Gir",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "354602",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "landed",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "CVG",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "432302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "JFK",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2018-11-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "AtlGHghas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "30dfs2",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "arrival",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "JFK",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "landed",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "CVG",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "432302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "arrival",
-        "status": "landed",
-        "departure": {
-          "iataCode": "ULY",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-13-11T01:08:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-03-11T06:30:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Gatlas Gir",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "354602",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "CVG",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "432302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "arrival",
-        "status": "landed",
-        "departure": {
-          "iataCode": "ULY",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-13-11T01:08:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-03-11T06:30:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Gatlas Gir",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "354602",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "arrival",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "ULY",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-13-11T01:08:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "VKO",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-03-11T06:30:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Gatlas Gir",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "354602",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "landed",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "CVG",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2017-12-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "Atlas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "432302",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      },
-      {
-        "type": "departure",
-        "status": "cancelled",
-        "departure": {
-          "iataCode": "VKO",
-          "icaoCode": "KJFK",
-          "scheduledTime": "2017-12-11T01:06:00.000",
-          "estimatedRunway": "2017-12-11T02:07:00.000",
-          "actualRunway": "2017-12-11T02:07:00.000"
-        },
-        "arrival": {
-          "iataCode": "JFK",
-          "icaoCode": "KCVG",
-          "estimatedRunway": "2017-12-11T03:38:00.000",
-          "actualRunway": "2018-11-11T03:38:00.000"
-        },
-        "airline": {
-          "name": "AtlGHghas Air",
-          "iataCode": "5Y",
-          "icaoCode": "GTI"
-        },
-        "flight": {
-          "number": "30dfs2",
-          "iataNumber": "5Y302",
-          "icaoNumber": "GTI302"
-        }
-      }
-      ]
-    }
+      data: []
+    };
+
+  }
+
+  componentDidMount() {
+    var arrivalPromise = fetch('http://aviation-edge.com/v2/public/timetable?key=b4c850-ddddeb&iataCode=VKO&type=arrival').then(res => res.json());
+    var departurePromise = fetch('http://aviation-edge.com/v2/public/timetable?key=b4c850-ddddeb&iataCode=VKO&type=departure').then(res => res.json());
+
+    Promise.all([arrivalPromise, departurePromise]).then(results => {
+      var commonResult = results[0].concat(results[1]);
+
+      this.initial = { data: commonResult };
+      this.setState({ data: this.initial.data });
+    }).catch(() => {
+      console.error('oh-oh');
+    });
+
   }
 
   render() {
@@ -890,7 +476,7 @@ class App extends Component {
           <div className="filter__type">
             <button className="filter__status" onClick={() => this.filterType('departure')}>Departure</button>
             <button className="filter__status" onClick={() => this.filterType('arrival')}>Arrival</button>
-            <button className="filter__status" onClick={() => this.filterType('cancelled')}>Cancelled</button>
+            <button className="filter__status" onClick={this.filterStatus}>Scheduled</button>
           </div>
           <input className="filter__search" placeholder="Search by flight number" onChange={this.filterFlights} />
         </div>
@@ -900,15 +486,23 @@ class App extends Component {
   }
 
   filterType(type) {
-    var arr = this.initial.data.filter(function (item) {
-      return item.type === type || item.status === type;
+    const arr = this.initial.data.filter(function (item) {
+      return item.type === type;
+    });
+    this.setState({ data: arr });
+  }
+
+  filterStatus() {
+    const arr = this.initial.data.filter(function (item) {
+      return item.status === 'scheduled';
     });
     this.setState({ data: arr });
   }
 
   filterFlights(event) {
     var searchResult = this.initial.data.filter(function (item) {
-      return item.flight.number.toString().toLowerCase().indexOf(event.currentTarget.value.toString().toLowerCase()) !== -1;
+      return item.flight.number
+        .toString().toLowerCase().indexOf(event.currentTarget.value.toString().toLowerCase()) !== -1;
     })
     this.setState({ data: searchResult });
   }
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -8,7 +8,7 @@ class Table extends React.Component {
             <table className="table">
                 <thead className="table__head">
                     <tr className="table__line">
-                        <th className="table__cell table__cell_main">Date</th>                    
+                        <th className="table__cell table__cell_main">Date</th>
                         <th className="table__cell table__cell_main">Time</th>
                         <th className="table__cell table__cell_main">Airport</th> 
                         <th className="table__cell table__cell_main">Flight</th>
@@ -23,18 +23,6 @@ class Table extends React.Component {
     }
 
     generateLine() {
-
-        var optionsDate = {
-            month: 'long',
-            day: 'numeric',
-          };
-
-        var optionsTime = {
-            timezone: 'UTC',
-            hour: 'numeric',
-            minute: 'numeric',
-          };
-
         var data = this.props.flightData;
 
         if (data.length === 0) {
@@ -43,30 +31,56 @@ class Table extends React.Component {
 
         var arr = [];
 
-        data.forEach(item => {
+        data.forEach((item, idx) => {
             if (item.type === 'arrival') {
                     arr.push(
-                        <tr className="table__line">
-                            <td className="table__cell">{new Date(Date.parse(item.arrival.estimatedRunway)).toLocaleString("en-US", optionsDate)}</td>                        
-                            <td className="table__cell">{new Date(Date.parse(item.arrival.estimatedRunway)).toLocaleString("en-US", optionsTime)}</td>
+                        <tr className="table__line" key={this.generateKey(item, idx)}>
+                            <td className="table__cell">{this.formatDate(item, 'estimatedRunway', 'arrival')}</td>
+                            <td className="table__cell">{this.formatDate(item, 'estimatedRunway', 'arrival', 'time')}</td>
                             <td className="table__cell">{item.departure.iataCode}</td>
                             <td className="table__cell">{item.flight.number}</td>
                             <td className="table__cell">{item.status}</td>
-                        </tr>)
-
+                        </tr>
+                    );
             } else if (item.type === 'departure') {
                     arr.push(
-                        <tr className="table__line">
-                            <td className="table__cell">{new Date(Date.parse(item.departure.scheduledTime)).toLocaleString("en-US", optionsDate)}</td>                        
-                            <td className="table__cell">{new Date(Date.parse(item.departure.scheduledTime)).toLocaleString("en-US", optionsTime)}</td>
+                        <tr className="table__line" key={this.generateKey(item, idx)}>
+                            <td className="table__cell">{this.formatDate(item, 'scheduledTime', 'departure')}</td>
+                            <td className="table__cell">{this.formatDate(item, 'scheduledTime', 'departure', 'time')}</td>
                             <td className="table__cell">{item.arrival.iataCode}</td>
                             <td className="table__cell">{item.flight.number}</td>
                             <td className="table__cell">{item.status}</td>
-                        </tr>)
+                        </tr>
+                    );
             }
         });
         
         return arr;
+    }
+
+    formatDate(item, fieldName, flightType, timeType) {
+        var optionsDate = {
+            month: 'long',
+            day: 'numeric',
+        };
+
+        var optionsTime = {
+            timezone: 'UTC',
+            hour: 'numeric',
+            minute: 'numeric',
+        };
+
+        if (!item[flightType][fieldName]) {
+            return '-';
+        }
+
+        return new Date(
+            Date.parse(item[flightType][fieldName]))
+            .toLocaleString("en-US",timeType === 'time' ? optionsTime : optionsDate);
+    }
+    
+    generateKey(item, idx) {
+        return item.flight.iataNumber + item.flight.icaoNumber + item.flight.number + idx;
     }
 }
 
